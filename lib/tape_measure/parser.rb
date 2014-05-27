b70e8dd3e82531c4d3f27f350bc1b8a6f2cfc37e
Sam Congleton <stcongleton@gmail.com>
2014-05-27 14:31:00 -0400

    commented out tests for step by step work through, created parser class

---
+++ lib/tape_measure/parser.rb
@@ -0,0 +1,32 @@
+module TapeMeasure
+  # Format numbers in mixed numbers
+  
+  class Parser
+    def initialize(string)
+
+      Citrus.load 'lib/tape_measure/length_grammar'
+
+      @string  = string
+      return nil if @number.blank?
+      return 0.0 if @number == 0.0
+
+      parse
+      output
+
+    end
+
+    def parse
+      @parsed = LengthGrammer.parse(string)
+    end
+
+    def output
+      begin
+        output = @parsed.value
+      rescue
+        output = output.error
+      ensure
+        output
+      end
+    end
+  end
+end
