9a3c20edea0157814742f7f903b4267605547328
Russell Osborne <russosborn@gmail.com>
2014-05-28 09:18:45 -0400

    Moving Citrus load out of the class to prevent multiple load events.

--- lib/tape_measure/parser.rb
+++ lib/tape_measure/parser.rb
@@ -1,22 +1,20 @@
+require 'citrus'
+require 'ruby_units/namespaced'
+Citrus.load 'lib/tape_measure/length_grammar'
+
 module TapeMeasure
   # Format numbers in mixed numbers
-  require 'citrus'
   class Parser
     def initialize(string)
       @string  = string
       return @string.blank? if @string == 0.0
-
       parse
-
     end
 
     def parse
-      Citrus.load 'lib/tape_measure/length_grammar'
-      begin
-        @parsed = LengthGrammar.parse(@string.strip).value
-      rescue => ex
-        ex.message
-      end
+      @parsed = LengthGrammar.parse(@string.strip).value
+    rescue => ex
+      ex.message
     end
   end
 end
